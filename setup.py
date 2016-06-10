be29e224a8f6ad4e4ae26c2048664ee56bbcb490
gfacciol <gfacciol@gmail.com>
2016-06-10 09:51:41 +0200

    disable DEBUG and assertions

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ import numpy as np
 files = ['pymaxflow.pyx', 'graph.cpp', 'maxflow.cpp']
 
 extensions = [Extension("pymaxflow", files, 
-   language="c++", include_dirs=[np.get_include()])]
+   language="c++", include_dirs=[np.get_include()], extra_compile_args=["-O3", "-DNDEBUG"] )]
 
 setup( ext_modules = cythonize(extensions))
